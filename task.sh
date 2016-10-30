1d4ed9d4a1748290c5b7368c75254ddf9c5d9bc1
Gor Martsen <Gor.martsen@gmail.com>
2016-10-30 17:54:11 -0400

    use = instead of :

--- task.sh
+++ task.sh
@@ -8,7 +8,7 @@ secret=`cat mock-client.config|grep secret|awk -F= '{print$2}'`
 name=`cat mock-client.config|grep name|awk -F= '{print$2}'`
 
 result=`curl -s $TASK_URL -X GET -H "Accept: application/json" -H "MAC:$mac"`
-url=`echo $result|grep URL|awk -F: '{print$2}'`
+url=`echo $result|grep URL|awk -F= '{print$2}'`
 
 cd /home/build/srpms
 wget $url

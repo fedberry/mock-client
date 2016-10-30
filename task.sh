58758cd2c1bf3488faba280eb0d58a047de362ae
Gor Martsen <Gor.martsen@gmail.com>
2016-10-30 17:21:37 -0400

    Config and task professor prototype

---
+++ task.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This is basic prototype.
+
+TASK_URL="http://mock.fedberry.org/mock_cluster/task"
+mac=`cat /sys/class/net/usb0/address`
+secret=`cat mock-client.config|grep secret|awk -F= '{print$2}'`
+name=`cat mock-client.config|grep name|awk -F= '{print$2}'`
+
+result=`curl -s $TASK_URL -X GET -H "Accept: application/json" -H "MAC:$mac"`
+url=`echo $result|grep URL|awk -F: '{print$2}'`
+
+cd /home/build/srpms
+wget $url
+
+rpmname=`basename $url`
+
+su -l build -c 'mock -r '$name' -n --rebuild ~/srpms/'$rpmname' '

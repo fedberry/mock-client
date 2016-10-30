52eebf607d0c17e7a8ded45a2cc92e219026e7ca
Gor Martsen <Gor.martsen@gmail.com>
2016-10-30 16:47:35 -0400

    Few tweaks on register

--- register.sh
+++ register.sh
@@ -5,14 +5,15 @@
 arch=`arch`
 secret=`date|md5sum |awk '{print$1}'`
 mac=`cat /sys/class/net/usb0/address`
+MOCK_SERVER="http://mock.fedberry.org/mock_cluster/register"
 
 echo '{"MAC":"'$mac'", "secret": "'$secret'", "arch": "'$arch'"}'
-result=`curl -s http://mock.fedberry.org/mock_cluster/register -X POST -H "Accept: application/json" -d '{"MAC":"'$mac'", "secret": "'$secret'", "arch": "'$arch'"}'`
+result=`curl -s $MOCK_SERVER -X POST -H "Accept: application/json" -d '{"MAC":"'$mac'", "secret": "'$secret'", "arch": "'$arch'"}'`
 
 
 echo $result
 if [ "$result" != "Agent registered." ]; then
   echo "Not registered"
 else
-  echo "Time to save $secret"
+  echo "Secret:$secret"
 fi

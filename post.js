9809a4304275570a0fb38045fa2a3b0b47412081
Gor Martsen <Gor.martsen@gmail.com>
2016-11-06 11:31:56 -0500

    debug script for post

---
+++ post.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node --harmony
+/**
+ * test Upload script.
+ */
+
+const taskId = 2;
+
+const fs = require('fs');
+require('dotenv-save').config({path: '/etc/mock-client/mock-client.config'});
+const ROOTDIR = '/home/mockclient/';
+
+postFromDir(ROOTDIR + '/tasks/' + taskId + '/result', /\.rpm$/)
+
+function postFromDir(startPath,filter){
+  if (!fs.existsSync(startPath)){
+      console.log("no dir ",startPath);
+      return;
+  }
+
+  var files = fs.readdirSync(startPath);
+  for(var i=0; i<files.length; i++){
+    var filename = path.join(startPath,files[i]);
+    var stat = fs.lstatSync(filename);
+    if (stat.isFile()){
+      if(filter.test(filename)) {
+        sendFile(filename);
+      };
+  };
+};
+
+const sendFile(file){
+  console.log('Found: ' + file);
+}
